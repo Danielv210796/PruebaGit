df2e8166647ed9cbde53449403408669d10718bf
CarlosAlb7 <71297869+CarlosAlb7@users.noreply.github.com>
2020-12-02 22:06:06 -0800

    Quite los anuncion del viwebag

--- PruebaGit/PruebaGit.Web/Controllers/HomeController.cs
+++ PruebaGit/PruebaGit.Web/Controllers/HomeController.cs
@@ -15,15 +15,13 @@ namespace PruebaGit.Web.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+           
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
+           
             return View();
         }
     }
