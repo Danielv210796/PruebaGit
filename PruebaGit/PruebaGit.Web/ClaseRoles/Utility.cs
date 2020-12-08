de47c2bbc43059c78c9851f2e00d70c4fc095074
CarlosAlb7 <71297869+CarlosAlb7@users.noreply.github.com>
2020-12-07 20:45:45 -0800

    Creacion de los roles

---
+++ PruebaGit/PruebaGit.Web/ClaseRoles/Utility.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using PruebaGit.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PruebaGit.Web.ClaseRoles
+{
+    public class Utility
+    {
+        readonly static ApplicationDbContext db = new ApplicationDbContext();
+        public static void CheckRoles(string rol)
+        {
+            var role = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            if (!role.RoleExists(rol))
+            {
+                role.Create(new IdentityRole(rol));
+            }
+        }
+    }
+}
