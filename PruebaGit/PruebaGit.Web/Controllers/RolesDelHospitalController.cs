981ccb836adaefbb7db45dc23141923ae47e9ff9
CarlosAlb7 <71297869+CarlosAlb7@users.noreply.github.com>
2020-12-13 23:11:01 -0800

    Roles Completados

--- PruebaGit/PruebaGit.Web/Controllers/RolesDelHospitalController.cs
+++ PruebaGit/PruebaGit.Web/Controllers/RolesDelHospitalController.cs
@@ -1,4 +1,7 @@
-﻿using PruebaGit.Web.Models;
+﻿
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using PruebaGit.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +10,7 @@ using System.Web.Mvc;
 
 namespace PruebaGit.Web.Controllers
 {
+    [Authorize (Roles ="Administrador")]
     public class RolesDelHospitalController : Controller
     {
         readonly static ApplicationDbContext db = new ApplicationDbContext();
@@ -15,6 +19,28 @@ namespace PruebaGit.Web.Controllers
         {
             var users = db.Users.ToList();
             return View(users);
+        }
+        //Metodo get ADD
+        public ActionResult AddRoles(string id)
+        {
+            var user = db.Users.Find(id);
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult AddRoles(UserViewModels uvm)
+        {
+            var userManager=new UserManager <ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var userRole = userManager.GetRoles(uvm.UserId);
+            if (userRole.Count>0)
+            {
+                userManager.RemoveFromRoles(uvm.UserId, userRole.ToArray());
+                userManager.AddToRole(uvm.UserId, uvm.RolName);
+            }
+            else
+            {
+                userManager.AddToRole(uvm.UserId, uvm.RolName);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
--- PruebaGit/PruebaGit.Web/Models/UserViewModels.cs
+++ PruebaGit/PruebaGit.Web/Models/UserViewModels.cs
@@ -7,8 +7,8 @@ namespace PruebaGit.Web.Models
 {
     public class UserViewModels
     {
-        public int UserId { get; set; }
-        public int UserName { get; set; }
-        public int RolName { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string RolName { get; set; }
     }
 }
