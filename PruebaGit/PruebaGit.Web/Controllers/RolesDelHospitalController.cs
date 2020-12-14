27bd28f08896b91466316a8278c7021c4735c537
CarlosAlb7 <71297869+CarlosAlb7@users.noreply.github.com>
2020-12-13 21:48:03 -0800

    Se agrego controlador y vista de los roles

---
+++ PruebaGit/PruebaGit.Web/Controllers/RolesDelHospitalController.cs
@@ -0,0 +1,20 @@
+﻿using PruebaGit.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaGit.Web.Controllers
+{
+    public class RolesDelHospitalController : Controller
+    {
+        readonly static ApplicationDbContext db = new ApplicationDbContext();
+        // GET: RolesDelHospital
+        public ActionResult Index()
+        {
+            var users = db.Users.ToList();
+            return View(users);
+        }
+    }
+}
