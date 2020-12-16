fe59eeff5edbe120bda7a981d5dd60470904e50e
CarlosAlb7 <71297869+CarlosAlb7@users.noreply.github.com>
2020-12-15 21:36:36 -0800

    Agregar foto

--- PruebaGit/PruebaGit.Web/Controllers/ProfileController.cs
+++ PruebaGit/PruebaGit.Web/Controllers/ProfileController.cs
@@ -8,34 +8,54 @@ using System.Web.Mvc;
 
 
 namespace PruebaGit.Web.Controllers
+public class ProfileController : Controller
 {
-    [Authorize]
-    public class ProfileController : Controller
+    readonly ApplicationDbContext db = new ApplicationDbContext();
+    // GET: Profile
+    public ActionResult Details()
     {
-        readonly ApplicationDbContext db = new ApplicationDbContext();
-        // GET: Profile
-        public ActionResult Details()
-        {
-            var userId = User.Identity.GetUserId();
-            var user = db.Users.Find(userId);
-            return View(user);
-        }
-
-        [HttpPost]
-        public  ActionResult Details(ProfileViewModel pvm)
-        {
-            var userId = User.Identity.GetUserId();
-            var userbd = db.Users.Find(userId);
-
-            userbd.Name = pvm.Name;
-            userbd.PhoneNumber = pvm.PhotoNumber;
-            userbd.Photo = pvm.Photo;
-            userbd.Videos = pvm.Videos;
-            userbd.Hobbies = pvm.Hobbies;
-            userbd.Descripcion = pvm.Descripcion;
-            db.SaveChanges();
-                return RedirectToAction("Details");
-
-        }
+        var userId = User.Identity.GetUserId();
+        var user = db.Users.Find(userId);
+        return View(user);
     }
+
+    [HttpPost]
+    public ActionResult Details(ProfileViewModel pvm)
+    {
+        var userId = User.Identity.GetUserId();
+        var userbd = db.Users.Find(userId);
+
+        userbd.Name = pvm.Name;
+        userbd.PhoneNumber = pvm.PhotoNumber;
+        userbd.Photo = pvm.Photo;
+        userbd.Videos = pvm.Videos;
+        userbd.Hobbies = pvm.Hobbies;
+        userbd.Descripcion = pvm.Descripcion;
+        db.SaveChanges();
+        return RedirectToAction("Details");
+
+    }
+    [HttpPost]
+    public ActionResult Picture(HttpPostedFileBase pic)
+    {
+
+        string path = Server.MapPath("~/Upload/");
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+        var userId = User.Identity.GetUserId();
+        var userdb = db.Users.Find(userId);
+        var photo = pic.FileName;
+        var dir = "";
+        if (pic != null)
+        {
+            dir = User.Identity.Name + Path.GetExtension(photo);
+            pic.SaveAs(path + dir);
+        }
+        userdb.Photo = dir;
+        db.SaveChanges();
+        return RedirectToAction("Details");
+    }
+
 }
